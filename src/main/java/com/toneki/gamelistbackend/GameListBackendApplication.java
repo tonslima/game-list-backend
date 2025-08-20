73c08f86674ce560d3da29b007eeb9705579c4aa
ton-home <ton.slima@icloud.com>
2025-08-19 23:41:08 -0300

    stated project

---
+++ src/main/java/com/toneki/gamelistbackend/GameListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toneki.gamelistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameListBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameListBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toneki/gamelistbackend/GameListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toneki.gamelistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
